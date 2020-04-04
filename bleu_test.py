867648dcde7cb6252b25cf95ec068625ed9082c6
Nkpdqz <529446386@qq.com>
2020-04-04 21:33:16 +0800

    bleu test

---
+++ bleu_test.py
@@ -0,0 +1,5 @@
+from nltk.translate.bleu_score import sentence_bleu
+reference = [['this', 'is', 'a', 'test'], ['this', 'is' 'test']]
+candidate = ['this', 'is', 'an', 'apple']
+score = sentence_bleu(reference, candidate)
+print(score)
